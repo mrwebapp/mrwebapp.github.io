61c37834f1bee8ae16e7c5d343667751dbac8782
mrwebapp <github.com@andi-isler.ch>
2014-11-23 16:32:30 +0100

    gemeinden csv file

---
+++ js/maps.js
@@ -0,0 +1,76 @@
+function initialize() {
+        var mapOptions = {
+          center: new google.maps.LatLng(-34.397, 150.644),
+          zoom: 8,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        var map = new google.maps.Map(document.getElementById("mapholder"),
+            mapOptions);
+      }
+
+
+function getLocation()
+  {
+  if (navigator.geolocation)
+	{
+	navigator.geolocation.getCurrentPosition(showPosition,showError);
+	}
+  else{x.innerHTML="Geolocation is not supported by this browser.";}
+  }
+
+function showPosition(position)
+  {
+  lat=position.coords.latitude;
+  lon=position.coords.longitude;
+  latlon=new google.maps.LatLng(lat, lon);
+  mapholder=document.getElementById('mapholder');
+  address=document.getElementById('address');
+
+  var myOptions={
+  center:latlon,zoom:13,
+  mapTypeId:google.maps.MapTypeId.ROADMAP,
+  mapTypeControl:false,
+   disableDefaultUI: true,
+	  //navigationControlOptions:{style:google.maps.NavigationControlStyle.SMALL}
+  };
+  var map=new google.maps.Map(mapholder,myOptions);
+  var marker=new google.maps.Marker({position:latlon,map:map,title:"You are here!"});
+	 geocoder = new google.maps.Geocoder();
+	 geocoder.geocode({'latLng': latlon}, function(results, status) {
+    if (status == google.maps.GeocoderStatus.OK) {
+      if (results[1]) {
+      address.innerHTML=results[1].address_components[0].long_name;
+      } else {
+        alert('No results found');
+      }
+    } else {
+      alert('Geocoder failed due to: ' + status);
+    }
+  });
+
+
+
+	  var geoloc=$.get("http://maps.googleapis.com/maps/api/geocode/json?latlng=40.714224,-73.961452&sensor=true")
+
+
+	  address.innerHTML= geoloc.results[1].formatted_address;
+  }
+
+function showError(error)
+  {
+  switch(error.code)
+	{
+	case error.PERMISSION_DENIED:
+	  x.innerHTML="User denied the request for Geolocation."
+	  break;
+	case error.POSITION_UNAVAILABLE:
+	  x.innerHTML="Location information is unavailable."
+	  break;
+	case error.TIMEOUT:
+	  x.innerHTML="The request to get user location timed out."
+	  break;
+	case error.UNKNOWN_ERROR:
+	  x.innerHTML="An unknown error occurred."
+	  break;
+	}
+  }

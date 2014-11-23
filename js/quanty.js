c78de57df1b0a32b9ec43a56df043cf20c1ba311
mrwebapp <github.com@andi-isler.ch>
2014-11-24 00:29:24 +0100

    zip als Golbale Variable, Suchbutton ergänzt

--- js/quanty.js
+++ js/quanty.js
@@ -2,6 +2,8 @@
 // General
 // ----------------------------------
 
+var zip;
+
 function toggle_visibility(id) {
        var e = document.getElementById(id);
        if(e.style.display == 'block')
@@ -12,12 +14,12 @@ function toggle_visibility(id) {
 
 var x = document.getElementById("error");
 
-function plz_gdenr(plz) {
+function plz_gdenr() {
 content=document.getElementById('content');
     $.getJSON( "./data/json/plz_gdenr.json" )
         .done(function( json ) {
-            content.innerHTML="test";
-//            content.innerHTML=json[2].4500;
+            content.innerHTML=json[0][zip];
+//            content.innerHTML=plz;
         })
         .fail(function( jqxhr, textStatus, error ) {
             var err = textStatus + ", " + error;
@@ -60,23 +62,23 @@ function showPosition(position)
 	 geocoder.geocode({'latLng': latlon}, function(results, status) {
      if (status == google.maps.GeocoderStatus.OK) {
       	if (results[1]) {
-      	address.innerHTML=results[0].formatted_address;
-//      address.innerHTML=results[1].address_components[0].long_name;
+     	//address.innerHTML=results[0].formatted_address;
+      zip=address.innerHTML=results[1].address_components[0].long_name;
 
 
    			//var zip = '4103';
-	 		var zip = results[0].address_components[8].long_name;
-	 		//$('div#content').text(zip);
-		  	if($.trim(zip) != ''){
-			  	$.post( "ajax/return.php", { zip: zip}, function(data) {
-		 	//$.post('ajax/return.php', 'zip: zip', function(data) {
-			 		$('div#content').text(data)});
-	  			} else {
-        			alert('No results found');
-	  			}
-    		} else {
-      			alert('Geocoder failed due to: ' + status);
-    		}
+//	 		zip = results[0].address_components[8].long_name;
+//	 		//$('div#content').text(zip);
+//		  	if($.trim(zip) != ''){
+//			  	$.post( "ajax/return.php", { zip: zip}, function(data) {
+//		 	//$.post('ajax/return.php', 'zip: zip', function(data) {
+//			 		$('div#content').text(data)});
+//	  			} else {
+//        			alert('No results found');
+//	  			}
+//    		} else {
+//      			alert('Geocoder failed due to: ' + status);
+	}
 	 }});
   }
 

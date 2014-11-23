ecb2b58600f00b4f233f46d1ebeed718132ba086
Tim Hagmann <tim.hagmann@gmail.com>
2014-11-23 16:35:29 +0100

    upload R

--- src/R/00_main.R
+++ src/R/00_main.R
@@ -1,28 +1,20 @@
 #!/usr/bin/Rscript
-# Purpose:         Analyse data
-# Date:            2014-10-30
-# Author:          tim.hagmann@baloise.ch
-# Notes:           In order for it to work on Windows, RTools() has to be installed
-# R Version:       R version 3.1.1 -- "Sock it to Me"
+# Purpose:         Analyse BFS Statistics
+# Date:            2014-11-23
+# Author:          Tim Hagmann
+# Notes:
+# R Version:       R version 3.1.2 -- "Pumpkin Helmet"
 ################################################################################
 
 ## Download init File
 # download.file(url="https://rawgit.com/greenore/initR/master/init.R",
-#               destfile="src/analysis/01_initialize.R",
+#               destfile="src/R/01_initialize.R",
 #               method=ifelse(Sys.info()["sysname"][[1]] == "Linux", "wget", "auto"))
 
 ## Source Files
 update_packages <- FALSE
-source("src/analysis/01_initialize.R")
-source("src/analysis/02_load.R")
-source("src/analysis/03_data.R")
+source("src/R/01_initialize.R")
+source("src/R/02_load.R")
+source("src/R/03_data.R")
+source("src/R/04_analysis.R")
 
-packagesGithub(c("visualizeR"), repo_name="greenore",
-               proxy_url="webproxy.balgroupit.com", port=3128,
-               auth_token="af8bd0071e5aef71efd5aded3aee60ec68f8a41f",
-               update=T)
-
-packagesGithub(c("beeplotR"), repo_name="greenore",
-               proxy_url="webproxy.balgroupit.com", port=3128,
-               auth_token="af8bd0071e5aef71efd5aded3aee60ec68f8a41f",
-               update=T)
--- src/R/02_load.R
+++ src/R/02_load.R
@@ -1,18 +1,17 @@
 ## Load Dependencies
 # Bioconductor
-required_packages <- c("EBImage")
-packagesBioconductor(required_packages, update=update_packages)
+# required_packages <- c("EBImage")
+# packagesBioconductor(required_packages, update=update_packages)
 
 # CRAN
-required_packages <- c("devEMF", "RSQLite")
+required_packages <- c("dplyr", "reshape2", "pxR")
 packagesCRAN(required_packages, update=update_packages)
 
 # Github
-required_packages <- c("visualizeR", "beeplotR", "transformR")
-packagesGithub(required_packages, repo_name="greenore",
-               proxy_url="webproxy.balgroupit.com", port=3128,
-               auth_token="af8bd0071e5aef71efd5aded3aee60ec68f8a41f",
-               update=update_packages)
+# required_packages <- c("visualizeR", "beeplotR", "transformR")
+# packagesGithub(required_packages, repo_name="greenore",
+#                proxy_url="webproxy.balgroupit.com", port=3128,
+#                update=update_packages)
 
 ## Clear Workspace
 rm(required_packages, update_packages)
